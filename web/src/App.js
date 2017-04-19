5053d9e427d1b483d7d55c41c25e02af6ca0b6df
Unknown <ania.jelin@wp.pl>
2017-04-19 16:35:08 +0200

    Stworzylismy web

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'purecss/build/pure-min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        ox
+      </div>
+    );
+  }
+}
+
+export default App;
